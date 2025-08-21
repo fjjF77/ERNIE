8fb7f82f96d556bc1817b69aaa4f53f30b5ba57c
fujinji <fujinji@baidu.com>
2025-08-21 11:26:19 +0800

    fix sentencepiece.bpe.model download

--- tests/utils/test_aistudio_download.py
+++ tests/utils/test_aistudio_download.py
@@ -12,52 +12,50 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# import unittest
+import unittest
 
-# from paddleformers.transformers.aistudio_utils import aistudio_download
-# from paddleformers.utils.download import resolve_file_path
-
-
-# class TestAistudioDownload(unittest.TestCase):
-#     def test_aistudio_download(self):
-#         # 设置测试数据
-#         repo_id = "PaddleNLP/DeepSeek-R1-Distill-Qwen-1.5B"
-#         filename = "model.safetensors"
-#         revision = "master"
-#         local_dir = "./local/model"
-
-#         # 调用待测试的函数
-#         result = resolve_file_path(
-#             repo_id=repo_id,
-#             filenames=filename,
-#             revision=revision,
-#             download_hub="aistudio",
-#             local_dir=local_dir,
-#         )
-
-#         # 验证结果
-#         print(result)
-#         self.assertEqual(result, f"{local_dir}/{filename}")
-
-#     def test_aistudio_download_transformer(self):
-#         # 设置测试数据
-#         repo_id = "PaddleNLP/DeepSeek-R1-Distill-Qwen-1.5B"
-#         filename = "model.safetensors"
-#         revision = "master"
-#         cache_dir = "./local/model"
-
-#         # 调用待测试的函数
-#         result = aistudio_download(
-#             repo_id=repo_id,
-#             filename=filename,
-#             revision=revision,
-#             cache_dir=cache_dir,
-#         )
-
-#         # 验证结果
-#         print(result)
-#         self.assertEqual(result, f"{cache_dir}/{filename}")
+from paddleformers.transformers.aistudio_utils import aistudio_download
+from paddleformers.utils.download import resolve_file_path
 
 
-# if __name__ == "__main__":
-#     unittest.main()
+class TestAistudioDownload(unittest.TestCase):
+    def test_aistudio_download(self):
+        # 设置测试数据
+        repo_id = "PaddleNLP/DeepSeek-R1-Distill-Qwen-1.5B"
+        filename = "model.safetensors"
+        revision = "master"
+        cache_dir = "./local/model"
+
+        # 调用待测试的函数
+        result = resolve_file_path(
+            repo_id=repo_id,
+            filenames=filename,
+            revision=revision,
+            download_hub="aistudio",
+            cache_dir=cache_dir,
+        )
+
+        # 验证结果
+        self.assertEqual(result, f"{cache_dir}/{repo_id}/{filename}")
+
+    def test_aistudio_download_transformer(self):
+        # 设置测试数据
+        repo_id = "PaddleNLP/DeepSeek-R1-Distill-Qwen-1.5B"
+        filename = "model.safetensors"
+        revision = "master"
+        cache_dir = "./local/model"
+
+        # 调用待测试的函数
+        result = aistudio_download(
+            repo_id=repo_id,
+            filename=filename,
+            revision=revision,
+            cache_dir=cache_dir,
+        )
+
+        # 验证结果
+        self.assertEqual(result, f"{cache_dir}/{filename}")
+
+
+if __name__ == "__main__":
+    unittest.main()
